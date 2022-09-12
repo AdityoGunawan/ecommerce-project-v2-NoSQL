4576d71acbdd7a72b1d80e40c0a8bb8fb8fa958a
adityo <muhammadadityogunawan@gmal.com>
2022-09-12 20:05:22 +0700

    Ready

---
+++ go.mod
@@ -0,0 +1,12 @@
+module ecommerce-v2
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-playground/validator/v10 v10.9.0
+	github.com/labstack/echo/v4 v4.9.0 // indirect
+	go.mongodb.org/mongo-driver v1.7.2
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+)
---
+++ middleware/middleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"net/http"
+
+	token "ecommerce-v2/tokens"
+
+	"github.com/gin-gonic/gin"
+	// "github.com/labstack/echo/v4"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No Authorization Header Provided"})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
